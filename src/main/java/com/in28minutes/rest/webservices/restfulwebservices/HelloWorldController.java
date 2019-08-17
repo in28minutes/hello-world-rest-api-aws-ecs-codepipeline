1f3b3bfeb17125618a35d921f50c132786a3d123
Ranga Rao Karanam <in28minutes@gmail.com>
2019-08-17 21:09:16 +0530

    a big change

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,12 +9,12 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World V3";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World - Changed - V2");
+		return new HelloWorldBean("Hello World - Changed - V3");
 	}
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
